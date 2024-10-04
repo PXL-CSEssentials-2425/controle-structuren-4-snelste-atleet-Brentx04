d6eb662de51783e3fbb19eacfb8b0b5b5058c76d
brentdaenen <Brent.Daenen@student.pxl.be>
2024-10-04 10:29:07 +0200

    laatste aanpassingen Atleet

---
+++ Oefening_Atleet/Oefening_Atleet/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Oefening_Atleet
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            nameTextBox.Clear();
+            timeTextBox.Clear();
+            resultsTextBox.Clear();
+
+            nameTextBox.Focus();
+        }
+
+        private void newButton_Click(object sender, RoutedEventArgs e)
+        {
+            int fastestTime;
+            string fastestName;
+
+            int currentTime = int.Parse(timeTextBox.Text);
+
+            if(fastestTime == 0 || currentTime < fastestTime)
+            {
+                fastestTime = currentTime;
+                fastestName = nameTextBox.Text;
+            }
+        }
+
+        private void fastestButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultsTextBox.Text + $"De snelste atleet is {fastestName} met een tijd van {fastestTime} seconden.";
+        }
+    }
+}
